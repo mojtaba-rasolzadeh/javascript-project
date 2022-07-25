8304906f56fff24d381543284d58c9ebbb13b779
mojtaba-rasolzadeh <mojtaba75rs@gmail.com>
2022-07-25 20:20:29 +0430

    Adding numbers to the screen and updating the screen

--- 2_ calculator/js/app.js
+++ 2_ calculator/js/app.js
@@ -1,5 +1,26 @@
 // class
-class Calculator {}
+class Calculator {
+    constructor(previousElement, currentElement) {
+        this.previousElement = previousElement;
+        this.currentElement = currentElement;
+        this.operation = undefined;
+        this.previousOperand = '';
+        this.currentOperand = '';
+    }
+
+    // Add number to display
+    appendNumber(number) {
+
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    // update display
+    updateDisplay() {
+        this.currentElement.textContent = this.currentOperand;
+        this.previousElement.textContent = this.previousOperand;
+    }
+
+}
 
 // varibales
 const previousElement = document.querySelector('.previous');
@@ -10,6 +31,15 @@ const operationBtns = document.querySelectorAll('[data-operation]');
 const numberBtns = document.querySelectorAll('.btn-number');
 const equalBtn = document.querySelector('#equal');
 
+const calculator = new Calculator(previousElement, currentElement);
+
+
 // event handler
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.dataset.input);
+        calculator.updateDisplay();
+    })
+})
 
 // functions
