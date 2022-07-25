d1c550d88dcaac8255a39f1c0237a6b8493fe356
mojtaba-rasolzadeh <mojtaba75rs@gmail.com>
2022-07-25 20:32:28 +0430

    Deleting the number and resetting the value of the variables

--- 2_ calculator/js/app.js
+++ 2_ calculator/js/app.js
@@ -5,12 +5,11 @@ class Calculator {
         this.currentElement = currentElement;
         this.operation = undefined;
         this.previousOperand = '';
-        this.currentOperand = '';
+        this.currentOperand = '0';
     }
 
     // Add number to display
     appendNumber(number) {
-
         this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
@@ -18,6 +17,23 @@ class Calculator {
     updateDisplay() {
         this.currentElement.textContent = this.currentOperand;
         this.previousElement.textContent = this.previousOperand;
+    }
+
+    // clear display
+    clear() {
+        this.previousOperand = '';
+        this.currentOperand = '';
+        this.operation = undefined;
+    }
+
+    // delete numbers 
+    delete() {
+        if (this.currentOperand.length == 1) {
+            this.currentOperand = '0';
+        } else {
+            this.currentOperand = this.currentOperand.toString().slice(0, -1);
+        }
+        return this.currentOperand;
     }
 
 }
@@ -30,16 +46,31 @@ const squareBtn = document.querySelector('#square');
 const operationBtns = document.querySelectorAll('[data-operation]');
 const numberBtns = document.querySelectorAll('.btn-number');
 const equalBtn = document.querySelector('#equal');
+const clearBtn = document.querySelector('#clear');
+const backspaceBtn = document.querySelector('#backspace');
 
 const calculator = new Calculator(previousElement, currentElement);
 
 
 // event handler
+// number buttons
 numberBtns.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.dataset.input);
         calculator.updateDisplay();
     })
+});
+
+// clear button 
+clear.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+// backspace button
+backspace.addEventListener('click', () => {
+    calculator.delete();
+    calculator.updateDisplay();
 })
 
 // functions
